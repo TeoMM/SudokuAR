96daa7b4f8cd7a0b7e996f74e87502a36020b3ab
Freeler <teodormoffice@gmail.com>
2021-04-20 00:34:04 +0300

    as

---
+++ components/camera_steps/adaptiveThreshold.js
@@ -0,0 +1,16 @@
+const threshold = (src) => {
+  // convert to grayscale
+  cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
+  // apply the
+  cv.adaptiveThreshold(
+    src,
+    src,
+    255,
+    cv.ADAPTIVE_THRESH_GAUSSIAN_C,
+    cv.THRESH_BINARY,
+    11,
+    10
+  );
+};
+
+export default threshold;
